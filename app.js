2b069a1fe206cfcd74a3d40ed122642e2bd6e08b
Akshaya Nagarajan <Akshaya Nagarajan>
2017-02-26 00:07:33 +0530

    Displaying nothing to buy message once

--- app.js
+++ app.js
@@ -4,30 +4,27 @@
 	app.service('ShoppingListCheckOffService', function() {
 
 		var toBuyArray = [{ name: "cookies", quantity: 10 },
-                  		  { name: "Apple", quantity: 5 },
-				  { name: "Orange", quantity: 9 },
-				  { name: "Noodles", quantity: 2 },
-	                          { name: "Veggie", quantity: 6 }];
+                  		{ name: "Apple", quantity: 5 },
+				  						{ name: "Orange", quantity: 9 },
+				  					  { name: "Noodles", quantity: 2 },
+	                    { name: "Veggie", quantity: 6 }];
 
-		var boughtArray = [{ name: "egg", quantity: 6 }];
-
-
+		var boughtArray = [];
 		this.returnToBuyArray = function () {
 		        return toBuyArray;
     		}
 		this.returnBoughtFunc = function (index) {
-			var newObj = [{}];			
+			var newObj = [{}];
 			newObj = toBuyArray.splice(index, 1);
 			boughtArray[boughtArray.length] = newObj[0];
+			return boughtArray;
 		}
 		this.returnBoughtArray = function () {
-		        return boughtArray;
+					return boughtArray;
     		}
 	});
 
-
 	app.controller('ToBuyController',['$scope','ShoppingListCheckOffService',function($scope,ShoppingListCheckOffService){
-
 		$scope.toBuyArray = ShoppingListCheckOffService.returnToBuyArray();
 		$scope.boughtFunc = function(name,quantity,index){
 			ShoppingListCheckOffService.returnBoughtFunc(index);
@@ -39,12 +36,17 @@
 
 
 	app.controller('AlreadyBoughtController',['$scope','ShoppingListCheckOffService',function($scope,ShoppingListCheckOffService){
-		
-		$scope.boughtArray = ShoppingListCheckOffService.returnBoughtArray();
-		if($scope.boughtArray.length == 0){
-			$scope.boughtArrayFlag = true;
-		}
-		console.log($scope.boughtArray.length);
+    $scope.alreadyBought = function(){
+			$scope.boughtArray = ShoppingListCheckOffService.returnBoughtArray();
+			console.log("Inside AlreadyBoughtController call "+$scope.boughtArray.length);
+			if($scope.boughtArray.length == 0){
+				return true;
+			}
+			else{
+				return false;
+			}
+	 }
+		// console.log("Bought Array "+$scope.boughtArray.length);
 	}]);
 
 })();
